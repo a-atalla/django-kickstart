f6f0bdc5343b499932181daca82607dafbdfbc77
a.atalla <a.3talla@gmail.com>
2023-08-05 22:54:59 +0300

    base model andcustom user model

--- my-project/core/admin.py
+++ my-project/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """Custom user admin"""
+
+    list_display = ["username", "email", "is_staff", "is_superuser"]
+
+    # New user form
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Core",
+            {"fields": ("email", "is_staff", "is_superuser")},
+        ),
+    )
--- my-project/core/models.py
+++ my-project/core/models.py
@@ -1,3 +1,22 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class BaseModel(models.Model):
+    """Abstract base model"""
+
+    uuid = models.UUIDField(default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Internal meta class for base model"""
+
+        abstract = True
+
+
 # Create your models here.
+class User(AbstractUser, BaseModel):
+    """Custom user model"""
